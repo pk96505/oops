2edb2513b0b2e353d6992d5b3a4e2cfbee8481e1
pk96505 <pankaj.kumar@iic.ac.in>
2017-08-06 23:07:08 +0530

    Aq2

--- Aq2.cpp
+++ Aq2.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 class Employee{
-static  int employee_id;
+	int employee_id;
+static  int nextid;
 	char employee_name[30];
 	int date;
 	int month;
@@ -11,8 +12,31 @@ static  int employee_id;
 	double salary;
 public:
 	Employee(){
-	employee_id=1700;
+	salary=25000;
 	}
 	void add_employee_detail();
 	void display_employee_detail();
+};
+
+int Employee::nextid=1700;
+
+void Employee::display_employee_detail(){
+	if(nextid==1700){
+	cout<<"There is no employee please add employee first."<<endl<<endl;
+	add_employee_detail();
+	}
+
+void Employee::add_employee_detail(char name,int date,int month,int year ){
 	
+	cout<<"Name of Employee: ";
+	gets(employee_name);
+	cout<<"Enter date of birth: "<<endl;
+	cout<<"Date: ";
+	cin>>date;
+	cout<<"Month: ";
+	cin>>month;
+	cout<<"Year: ";
+	cin>>year;
+	
+
+
