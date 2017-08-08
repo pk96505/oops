63bb8e8e6dc7012b8c11daf94631b1e242e3128e
Pankaj Kumar <pankaj.kumar@iic.ac.in>
2017-08-08 12:52:56 +0530

    Aq1

--- Aq1.cpp
+++ Aq1.cpp
@@ -19,17 +19,45 @@ public:
 long Bank_Account::c_acc_no=341310100000;
 
 int main(){
-	Bank_Account acc;
-	acc.create_acc();
-	return 0;
+	
+	int x=0;
+	int c;
+	do{
+	Bank_Account acc[x];
+	cout<<"1.To create New Account."<<endl;
+	cout<<"2.To deposit in Account."<<endl;
+	cout<<"3.To withdraw from Account."<<endl;
+	cout<<"4.To display your Account detail"<<endl;
+	cout<<"5.Exit."<<endl;
+	cout<<"  Enter your choice : ";
+	cin>>c;
+	switch(c){
+	case 1: 
+	acc[x].create_acc();
+	x++;
+	break;
+	case 2:
+	acc[x].deposit_amount();
+	break;
+	case 3:
+	acc[x].withdraw();
+	break;
+	case 4:
+	acc[x].display();
+	break;
+	case 5:
+	exit(0);
 	}
+	}while(true);
+	return 0;
+}
 
 
 void Bank_Account::create_acc(){
 	cout<<"Enter your name : ";
-	gets(c_name);
+	cin>>c_name;
 	cout<<"Enter account type Saving/Current :";
-	gets(acc_type);
+	cin>>acc_type;
 	display();
 	c_acc_no++;
 }
@@ -51,10 +79,11 @@ void Bank_Account::withdraw(){
 }
 
 void Bank_Account::display(){
-	cout<<c_name;
-	cout<<c_acc_no;
-	cout<<acc_type;
-	cout<<balance;
+	
+	cout<<"Account holder name: "<<c_name<<endl;
+	cout<<"Account number: "<<c_acc_no<<endl;
+	cout<<"Accoutn type: "<<acc_type<<endl;
+	cout<<"Total balance: "<<balance<<endl;
 }
 	
 	
