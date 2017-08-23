2d3c29e6d00ae5c17c099e5ca3343db2c7c134f9
pk96505 <pankaj.kumar@iic.ac.in>
2017-08-23 12:23:26 +0530

    q7.5.cpp

--- q7.5.cpp
+++ q7.5.cpp
@@ -5,31 +5,64 @@
 
 using namespace std;
 
-class Point{
-  float x;
-  float y;
-  float a;
-  float r;
+class Polar;
+
+class Rectangle{
+  double x;
+  double y;
 public:
-  void convert_p_to_r(){
-    a = atan(y/x);
-    r = sqrt(x*x + y*y);
-  }
-  void convert_r_to_p(){
-    x = r * cos(a);
-    y = r * sin(a);
-  }
-  friend ostream & operator << (ostream &out, const Point &P);
-  friend istream & operator >> (istream &in, Point &P);
+  friend Polar convert_p_to_r( Polar &P);
+  friend Rectangle convert_r_to_p( Rectangle &R);
+  friend ostream & operator << (ostream &out, const Rectangle &P);
+  friend istream & operator >> (istream &in, Rectangle &P);
+
 };
 
-ostream & operator << (ostream &out, const Point &P){
+class Polar{
+	double a;
+	double r;
+public:
+	friend Polar convert_p_to_r( Polar &P);
+	friend Rectangle convert_r_to_p( Rectangle &R);
+	friend ostream & operator << (ostream &out, const Polar &P);
+  	friend istream & operator >> (istream &in, Polar &P);
+}
+
+Polar convert_p_to_r( Polar &P ){
+    Rectangle R;
+    R.x = P.r * cos (a);
+    R.y = P.r * sin (a);
+    return R;
+}
+
+Rectangle convert_r_to_p( Rectangle &R ){
+	Polar P;
+	P.a = atan(R.y/R.x);
+    P.r = sqrt(R.x*R.x + R.y*R.y);
+    return P;
+}
+
+ostream & operator << (ostream &out, const Rectangle &R){
+    out<<"  x coordinate = "<<R.x<<endl;
+    out<<"  y coordinate = "<<R.y<<endl;
+    return out;
+}
+
+istream & operator >> (istream &in, Polar &P){
+    cout << "  Enter the value in degrees :";
+    in >> P.a;
+    cout << "  Enter the value in radians :";
+    in >> P.r;
+    return in;
+}
+
+ostream & operator << (ostream &out, const Polar &P){
     out<<"  In degrees = "<<P.a<<endl;
     out<<"  In radians = "<<P.r<<endl;
     return out;
 }
 
-istream & operator >> (istream &in, Point &P){
+istream & operator >> (istream &in, Rectangle &P){
     cout << "  Enter the value of x coordinate :";
     in >> P.x;
     cout << "  Enter the value of y coordinate :";
@@ -39,10 +72,23 @@ istream & operator >> (istream &in, Point &P){
  
 int main(){
   int c;
-  Point p;
-  
-  cin >> p;
-  p.convert();
-  cout << p;
-  return 0;
+  cout<<"  1.Convert rectangle to polar."<<endl;
+  cout<<"  2.Convert polar to rectangle."<<endl;
+  cout<<"  Enter your choice: ";
+  switch(c){
+  	case 1:
+  	Rectangle R;
+  	cin>>R;
+  	Rectangle A;
+  	A = convert_r_to_p(R);
+  	cout<<A;
+  	break;
+  	case 2:
+  	Polar P;
+  	cin>>P;
+  	Polar A1;
+  	A1 = convert_p_to_r(P);
+  	cout<<A1;
+  	break;
+  }
 }
