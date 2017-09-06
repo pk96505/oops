0ae3f2bcd7ad691b3d48cb879b301f59c1828ef1
Pankaj Kumar <pankaj.kumar@iic.ac.in>
2017-09-06 18:24:02 +0530

    q8.1

--- q8.1.cpp
+++ q8.1.cpp
@@ -31,15 +31,16 @@ class cur_acct:public account{
 	int cheque_no_f;
 	int cheque_no_t;
 	int acc_no_c;
-	char *withd_status_c;
 	float balance_c;
 public:
 	void putdetail_c();
+	int cheque_no();
 	void deposit_amount();
 	void issue();
-	void withd_cheque(int cheque, char *withd_s_cheq);
-	void withd_cash(char *withd_s_c);
+	void withd_cheque(int cheque);
+	void withd_cash();
 	void check_bal(float min_bal);
+	void display_bala_c();
 	int check_acc_no(){
 	return acc_no_c;
 	}
@@ -56,11 +57,23 @@ public:
 	void deposit_amount();
 	void calc_ci();
 	void withdraw();
+	void display_bala_s();
 	int check_acc_no(){
 	return acc_no_s;
 	}
 	void display_s();
 };
+
+int cur_acct::cheque_no(){
+	return cheque_no_f;
+}
+
+void cur_acct::display_bala_c(){
+	cout<<endl<<"*******************************************"<<endl;
+	display();
+	cout<<"  Total Balance is : "<<balance_c<<endl<<endl;
+	cout<<endl<<"*******************************************"<<endl;
+}
 
 void cur_acct::putdetail_c(){
 	char name[30],type[10];
@@ -84,14 +97,20 @@ void cur_acct::display_c(){
 	display();
 	cout<<"  Account number : "<<acc_no_c<<endl;
 	cout<<"  Total Balance : "<<balance_c<<endl;
+	if(cheque_no_f == cheque_no_t){
+		cout<<"  Cheque no. : "<<cheque_no_f<<" is cheque so please issue a new cheque book."<<endl;
+	}
+	else
+	   {
 	cout<<"  Cheque Book no. From : "<<cheque_no_f<<" to "<<cheque_no_t<<endl;
+	}
 	cout<<"*******************************************"<<endl;
 }
 
 void cur_acct::check_bal(float min_bal){
 	if(min_bal<5000){
 		cout<<"  Your account has gone below minimum balance required. That is 5000."<<endl<<endl;
-		balance_c-=100;
+		balance_c -= 100;
 	}
 }
 
@@ -99,24 +118,21 @@ void cur_acct::issue(){
 	cheque_no_f -= 10; 
 }
 
-void cur_acct::withd_cheque(int cheque, char *withd_s_cheq){
+void cur_acct::withd_cheque(int cheque){
 	float amount;
-	if(cheque>cheque_no_t){
-		cout<<"  Please issue a new cheque book."<<endl;
-		issue();
-	}
-	else
-		{
-		int len = strlen (withd_s_cheq);
-		withd_status_c = new char[len +1];
-		strcpy(withd_status_c,withd_s_cheq);
-		cheque_no_f++;
 		cout<<"   Enter the amount to withdraw from acccount : ";
 		cin>>amount;
 		balance_c-=amount;
 		check_bal(balance_c);
+		if(cheque == cheque_no_t){
+			cout<<"  This was your last cheque, So please issue new cheque book."<<endl;
+			issue();
+		}
+		else
+		{
+			cheque_no_f++;
+		}
 		display_c();
-	}
 }
 
 void cur_acct::deposit_amount(){
@@ -127,16 +143,20 @@ void cur_acct::deposit_amount(){
 	display_c();
 }
 
-void cur_acct::withd_cash(char *withd_s_c){
+void cur_acct::withd_cash(){
 	float amount;
-	int len = strlen (withd_s_c);
-	withd_status_c = new char[len +1];
-	strcpy(withd_status_c,withd_s_c);
 	cout<<"   Enter the amount to withdraw from acccount : ";
 	cin>>amount;
 	balance_c-=amount;
 	check_bal(balance_c);
 	display_c();
+}
+
+void sav_acct::display_bala_s(){
+	cout<<endl<<"*******************************************"<<endl;
+	display();
+	cout<<"  Total Balance is : "<<balance_s<<endl<<endl;
+	cout<<endl<<"*******************************************"<<endl;
 }
 
 void sav_acct::putdetail_s(){
@@ -170,7 +190,7 @@ void sav_acct::calc_ci(){
 	display_s();
 	balance_s+=ci; 
 	cout<<"   Compound Interest is : "<<ci<<endl;
-	cout<<"   Main balance is : "<<balance_s<<endl;
+	cout<<"   Main balance is : "<<balance_s<<endl<<endl<<endl;
 }
 
 void sav_acct::deposit_amount(){
@@ -190,7 +210,7 @@ void sav_acct::withdraw(){
 }
 
 int main(){
-	int type;
+	int type,c_c;
 	int account_no;
 	float amount;
 	int c=2,s=1,ch,j;
@@ -203,9 +223,9 @@ int main(){
 	s_a[j].putdetail_s();
 	}
 	do{
-		cout<<"  1.Deposit money in account."<<endl;
+		cout<<endl<<endl<<"  1.Deposit money in account."<<endl;
 		cout<<"  2.Display the balance."<<endl;
-		cout<<"  3.Compute and deposit Interest."<<endl;
+		cout<<"  3.Compute and deposit Interest in Saving account."<<endl;
 		cout<<"  4.Withdraw money from account."<<endl;
 		cout<<"  5.Display detail of account holder."<<endl;
 		cout<<"  6.Exit."<<endl<<endl;
@@ -214,8 +234,6 @@ int main(){
 		switch(ch){
 			case 1 :
 			cout<<endl<<"  Enter account type 1 for current and 2 for savings : ";
-			/*cin.ignore(-1);
-			cin.getline(type,10);*/
 			cin>>type;
 			if(type==1){
 				cout<<endl<<"  Please enter your account no. : ";
@@ -241,6 +259,91 @@ int main(){
 						cout<<"  Please enter correct account type. Please try again later......"<<endl;
 					}
 					break;
+
+			case 2 :
+			cout<<endl<<"  Enter account type 1 for current and 2 for savings : ";
+			cin>>type;
+			if(type==1){
+				cout<<endl<<"  Please enter your account no. : ";
+				cin>>account_no;
+				for(j=0; j<c; j++){
+					if(c_a[j].check_acc_no()==account_no)
+					break;
+				}
+				c_a[j].display_bala_c();
+				}
+				else
+					if(type==2){
+						cout<<endl<<"  Please enter your account no. : ";
+						cin>>account_no;
+						for(j=0; j<s; j++){
+							if(s_a[j].check_acc_no()==account_no)
+							break;
+						}
+						s_a[j].display_bala_s();
+					}
+					else
+					{
+						cout<<"  Please enter correct account type. Please try again later......"<<endl;
+					}
+					break;
+			
+			case 3 :
+			cout<<endl<<"  Please enter your account no. : ";
+			cin>>account_no;
+			for(j=0; j<c; j++){
+				if(s_a[j].check_acc_no()==account_no)
+				break;
+				}
+				s_a[j].calc_ci();
+				break;
+
+			case 4 :
+			cout<<endl<<"  Enter account type 1 for current and 2 for savings : ";
+			cin>>type;
+			if(type==1){
+				cout<<"  1.By Cheque."<<endl;
+				cout<<"  2.By Self."<<endl;
+				cout<<"   Enter your choice : ";
+				cin>>c_c;
+				switch(c_c){
+					case 1 :
+					cout<<endl<<"  Please enter your account no. : ";
+					cin>>account_no;
+					for(j=0; j<c; j++){
+					if(c_a[j].check_acc_no()==account_no)
+					break;
+					}
+				    c_a[j].withd_cheque(c_a[j].cheque_no());
+				    break;
+
+					case 2 :
+					cout<<endl<<"  Please enter your account no. : ";
+					cin>>account_no;
+						for(j=0; j<c; j++){
+					if(c_a[j].check_acc_no()==account_no)
+					break;
+					}
+					c_a[j].withd_cash();
+					break;
+				}
+			}
+				else
+					if(type==2){
+						cout<<endl<<"  Please enter your account no. : ";
+						cin>>account_no;
+						for(j=0; j<s; j++){
+							if(s_a[j].check_acc_no()==account_no)
+							break;
+						}
+						s_a[j].withdraw();
+					}
+					else
+					{
+						cout<<"  Please enter correct account type. Please try again later......"<<endl;
+					}
+					break;
+
 			case 5 :
 			for(int i=0; i<c; i++){
 				c_a[i].display_c();
@@ -249,6 +352,7 @@ int main(){
 				s_a[i].display_s();
 			}
 			break;
+
 			case 6 :
 			exit(0);
 			}
