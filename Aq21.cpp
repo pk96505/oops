609cc5120cf17455d6f884a13d9513c9ac93ff54
Pankaj Kumar <pankaj.kumar@iic.ac.in>
2017-09-06 19:00:25 +0530

    Aq21

---
+++ Aq21.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+class Date{
+	int dd,mm,yyyy;
+public:
+	Date(int d, int m,int y){
+		dd = d;
+		mm = m;
+		yyyy = y;
+	}
+};
+
+class Employee{
+	char *emp_name;
+	int emp_id;
+	float salary;
+	Date dob;
+public:
+	Employee(char *n, int i, int s, int d, int m, int y){
+		int len = strlen()
+	}
+
+}
