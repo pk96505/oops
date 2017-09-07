b34a4d847d02c10b145d6a16e96739cc39d8c54b
Pankaj Kumar <pankaj.kumar@iic.ac.in>
2017-09-07 17:08:44 +0530

    Aq21

--- Aq21.cpp
+++ Aq21.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <cmath>
 
+using namespace std;
+
 class Date{
 	int dd,mm,yyyy;
 public:
@@ -10,6 +12,10 @@ public:
 		dd = d;
 		mm = m;
 		yyyy = y;
+	}
+
+	void display_d(){
+		cout<<"  Date of Birth : "<<dd<<"/"<<mm<<"/"<<yyyy<<endl;
 	}
 };
 
@@ -19,8 +25,23 @@ class Employee{
 	float salary;
 	Date dob;
 public:
-	Employee(char *n, int i, int s, int d, int m, int y){
-		int len = strlen()
+	Employee(char *n, int i, int s, int d, int m, int y) : dob(d,m,y){
+		int len = strlen(n);
+		emp_name = new char [len + 1];
+		strcpy(emp_name,n);
+		emp_id = i;
+		salary = s;
 	}
 
+	void display(){
+		cout<<"  Name : "<<emp_name<<endl;
+		cout<<"  ID : "<<emp_id<<endl;
+		cout<<"  Salary : "<<salary<<endl;
+		dob.display_d();
+	}
+};
+
+int main(){
+	Employee e("Pankaj",101,20000,25,01,1994);
+	e.display();
 }
