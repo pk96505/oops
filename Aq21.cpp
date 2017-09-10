3615d921b9d6844361e334493ade7ad21aec1ddd
pk96505 <pankaj.kumar@iic.ac.in>
2017-09-11 03:22:55 +0530

    Aq21

--- Aq21.cpp
+++ Aq21.cpp
@@ -5,13 +5,21 @@
 
 using namespace std;
 
+int emp =0;
+int id = 1;
+int salary = 25000;
+
 class Date{
 	int dd,mm,yyyy;
 public:
-	Date(int d, int m,int y){
-		dd = d;
-		mm = m;
-		yyyy = y;
+	void putdob(){
+		cout<<"  Enter Date of Birth : "<<endl;
+		cout<<"  Enter Date : ";
+		cin>>dd;
+		cout<<"  Enter Month : ";
+		cin>>mm;
+		cout<<"  Enter Year : ";
+		cin>>yyyy;
 	}
 
 	void display_d(){
@@ -20,17 +28,31 @@ public:
 };
 
 class Employee{
-	char *emp_name;
+protected:
+	char emp_name[30];
 	int emp_id;
-	float salary;
+	float emp_salary;
 	Date dob;
 public:
-	Employee(char *n, int i, int s, int d, int m, int y) : dob(d,m,y){
+/*	Employee(char *n, int i, int s, int d, int m, int y) : dob(d,m,y){
 		int len = strlen(n);
 		emp_name = new char [len + 1];
 		strcpy(emp_name,n);
 		emp_id = i;
 		salary = s;
+	}
+*/
+	void addemployees_detail(){
+		//char name[30];
+		cout<<"  Enter the name of Employee : ";
+		//cin.ignore(-1);
+		//cin.getline(name,30);
+		//strcpy(emp_name,name);
+		cin>>emp_name;
+		emp_id = id;
+		emp_salary = salary;
+		dob.putdob();
+		id++;
 	}
 	void setsalary(int s){
 		salary = s;
@@ -40,7 +62,7 @@ public:
 		return salary;
 	}
 
-	void display(){
+	void display_emp_detail(){
 		cout<<"  Name : "<<emp_name<<endl;
 		cout<<"  ID : "<<emp_id<<endl;
 		cout<<"  Salary : "<<salary<<endl;
@@ -48,30 +70,72 @@ public:
 	}
 };
 
-class Manager{
+class Manager:protected Employee{
 	float bonus;
+	int no_emp;
 	Employee e[5];
 public:
+	int i;
 	void getbonus();
-	void setbonus();
-	void addemployees_managed();
-	void getemployees_managed();
+	void setbonus(int bonus);
+	void addmanager_detail(char * n){
+		char c;
+		strcpy(emp_name,n);
+		emp_id = id;
+		cout<<"  Enter bonus : ";
+		cin>>bonus;
+		cout<<"  Do you want to add employee to the Manager (Y/N) : ";
+		cin>>c;
+		if(c=='Y'||c=='y'){
+			addemployees_managed();
+		}
+		else{
+			no_emp = emp;
+		}
+		setbonus(bonus);
+		dob.putdob();
+	}
+	void addemployees_managed(){
+		int m;
+		cout<<"  How many employee do you want to add to this Manager (Max. is 5) : ";
+		cin>>m;
+		for(i=0; i<m; i++){
+			e[i].addemployees_detail();
+			e[i].display_emp_detail();
+			no_emp = emp+1;
+			emp++;
+		}
+	}
+	
+	void getemployees_managed(){
+		for(int j=0; j<i; j++){
+			e[j].display_emp_detail();
+		}
+	}
+
+	void display_m(){
+		display_emp_detail();
+		cout<<"  Total Employee managed by Manager is : "<<no_emp<<endl;
+	}
 };
 
-void Manager::addemployees_managed(){
-	
+void Manager::getbonus(){
+	cout<<"  Bonus is : "<<bonus<<endl;
+	cout<<"  Total salary is : "<<getsalary();
 }
 
-void Manager::setbonus(){
+void Manager::setbonus(int bonus){
 	float s,total_salary=0;
-	cout<<"  Enter bonus of Manager : ";
-	cin>>bonus;
 	s = getsalary();
 	total_salary = s+bonus;
 	setsalary(total_salary);
 }
 
 int main(){
-	Employee e("Pankaj",101,20000,25,01,1994);
-	e.display();
+	char n[30];
+	Manager m;
+	cout<<"  Enter name of Manager : ";
+	cin>>n;
+	m.addmanager_detail(n);
+	m.display_m();
 }
