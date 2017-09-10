c89f60321e9cc57fed8fa025507d9fc66df08e37
pk96505 <pankaj.kumar@iic.ac.in>
2017-09-11 00:17:56 +0530

    Aq22

---
+++ Aq22.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+class person{
+protected:
+	char * name;
+	int id;
+public:
+	person(const char *n, int i):id(i){
+		int len = strlen(n);
+		name = new char[len+1];
+		strcpy(name,n);
+	}
+
+	~person(){
+		cout<<"  Person is deleted."<<endl;
+	}
+
+	void display_p(){
+		cout<<"  Name : "<<name<<endl;
+		cout<<"  ID : "<<id<<endl;
+	}
+};
+
+class account:virtual protected person{
+protected:
+	int pay;
+public:
+	account(const char * n, int i, int p):person(n,i),pay(p){};
+	~account(){
+		cout<<"  Account is deleted."<<endl;
+	}
+	void display_a(){
+		display_p();
+		cout<<"  Pay : "<<pay<<endl;
+	}
+};
+
+class admin:virtual protected person{
+protected:
+	int experience;
+public:
+	admin(const char * n, int i, int e):person(n,i){
+		experience = e;
+	}
+
+	~admin(){
+		cout<<"  Admin is deleted."<<endl;
+	}
+
+	void display_ad(){
+		display_p();
+		cout<<"  Experience : "<<experience<<endl;
+	}
+};
+
+class master:protected account, protected admin{
+
+public:
+	master(const char * n, int i, int p, int e):account(n,i,p),admin(n,i,e),person(n,i){};
+	~master(){
+		cout<<"  Master is deleted."<<endl;
+	}
+	void display(){
+		cout<<"  Name : "<<name<<endl;
+		cout<<"  ID : "<<id<<endl;
+		cout<<"  Pay : "<<pay<<endl;
+		cout<<"  Experience : "<<experience<<endl;
+	}
+};
+int main(){
+	master m("Pankaj",144,10,30000);
+	m.display();	
+}
