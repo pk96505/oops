d5ec083afd3c0c4ba15e7c921945ba24bbed6371
Pankaj Kumar <pankaj.kumar@iic.ac.in>
2017-09-13 15:14:10 +0530

    Practical_1.pdf

--- q7.5.cpp
+++ q7.5.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -9,37 +8,37 @@ using namespace std;
 class Polar;
 
 class Rectangle{
-  double x;
-  double y;
+    double x;
+    double y;
 public:
-  	friend Rectangle convert_p_to_r( Polar &P);
-  	friend Polar convert_r_to_p( Rectangle &R);
-  	friend ostream & operator << (ostream &out, const Rectangle &P);
-  	friend istream & operator >> (istream &in, Rectangle &P);
+    friend Rectangle convert_p_to_r( Polar &P);
+    friend Polar convert_r_to_p( Rectangle &R);
+    friend ostream & operator << (ostream &out, const Rectangle &P);
+    friend istream & operator >> (istream &in, Rectangle &P);
 };
 
 class Polar{
-	double a;
-	double r;
+    double a;
+    double r;
 public:
-	friend Rectangle convert_p_to_r( Polar &P);
-	friend Polar convert_r_to_p( Rectangle &R);
-	friend ostream & operator << (ostream &out, const Polar &P);
-  friend istream & operator >> (istream &in, Polar &P);
+    friend Rectangle convert_p_to_r( Polar &P);
+    friend Polar convert_r_to_p( Rectangle &R);
+    friend ostream & operator << (ostream &out, const Polar &P);
+    friend istream & operator >> (istream &in, Polar &P);
 };
 
 Rectangle convert_p_to_r( Polar &P ){
-  Rectangle R;
-  R.x = P.r * cos (P.a);
-  R.y = P.r * sin (P.a);
-  return R;
+    Rectangle R;
+    R.x = P.r * cos (P.a);
+    R.y = P.r * sin (P.a);
+    return R;
 }
 
 Polar convert_r_to_p( Rectangle &R ){
-	Polar P;
-	P.a = atan(R.y/R.x);
-  P.r = sqrt(R.x*R.x + R.y*R.y);
-  return P;
+    Polar P;
+    P.a = atan(R.y/R.x);
+    P.r = sqrt(R.x*R.x + R.y*R.y);
+    return P;
 }
 
 ostream & operator << (ostream &out, const Rectangle &R){
@@ -71,38 +70,40 @@ istream & operator >> (istream &in, Rectangle &P){
     in >> P.y;
     return in;
 }
- 
+
 int main(){
-  int c;
-  do{
-    cout<<endl<<"  1.Convert rectangle to polar."<<endl;
-    cout<<"  2.Convert polar to rectangle."<<endl;
-    cout<<"  3.Exit."<<endl<<endl;
-    cout<<"  Enter your choice: ";
-    cin>>c;
-    switch(c){
-  	 case 1:
-  	  Rectangle R;
-  	  cin>>R;
-  	  Polar P1;
-  	  P1=convert_r_to_p(R);
-  	  cout<<P1;
-  	  break;
-  	   
-      case 2:
-  	  Polar P;
-  	  cin>>P;
-  	  Rectangle R1;
-  	  R1=convert_p_to_r(P);
-  	  cout<<R1;
-  	  break;
+    int c;
+    do{
+        cout<<endl<<"  1.Convert rectangle to polar."<<endl;
+        cout<<"  2.Convert polar to rectangle."<<endl;
+        cout<<"  3.Exit."<<endl<<endl;
+        cout<<"  Enter your choice: ";
+        cin>>c;
+        switch(c){
 
-      case 3:
-      exit(0);
-      break;
-
-      default :
-      cout<<"  You enter a worng choice. Please try again later or select correct choice."<<endl;
-    }
-  }while(true);
+            case 1 :
+                Rectangle R;
+                cin>>R;
+                Polar P1;
+                P1=convert_r_to_p(R);
+                cout<<P1;
+                break;
+                
+            case 2 :
+                Polar P;
+                cin>>P;
+                Rectangle R1;
+                R1=convert_p_to_r(P);
+                cout<<R1;
+                break;
+                
+            /*case 3 :
+                exit(0);
+                break;
+                
+            default:
+                cout<<"  You enter a worng choice. Please try again later or select correct choice."<<endl;
+                break;*/
+        }
+    }while(true);
 }
